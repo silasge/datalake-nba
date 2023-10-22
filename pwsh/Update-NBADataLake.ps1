1fcf9309e12d8d02ba3feb982a9ff2d239b8e33b
Silas Genário <silasge.dev@gmail.com>
2023-10-22 11:57:46 -0300

    fix: small errors in Update-NBADataLake.PS1

--- pwsh/Update-NBADataLake.ps1
+++ pwsh/Update-NBADataLake.ps1
@@ -5,7 +5,7 @@ function Update-NBADataLake {
         [switch]$ShotChartDetail
     )
 
-    $nba_bronze = "poetry run get_nba_bronze -sy $($SeasonYear) -st $($SeasonType)"
+    $nba_bronze = "poetry run get_nba_bronze -sy '$($SeasonYear)' -st '$($SeasonType)'"
 
     if ($ShotChartDetail.IsPresent) {
         $nba_bronze = $nba_bronze + " -shotchartdetail"
@@ -14,7 +14,12 @@ function Update-NBADataLake {
     Invoke-Expression $nba_bronze
 
     if (((Get-Date) - ((Get-Item db/nba.duckdb.dvc).LastWriteTime)).Day -gt 15) {
-        poetry run dvc add .\db\nba.duckdb
+        git checkout test/no-ref/datalake-update
+        poetry run dvc add db\nba.duckdb
         poetry run dvc push
+        git add db\nba.duckdb.dvc
+        git commit -m "test: database updated until $((Get-Date).ToString("yyyy-MM-dd HH:mm"))"
+        git push
+        git checkout main
     }
 }
